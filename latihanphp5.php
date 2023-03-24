c9f4e5bfbbf06bf08f4e5fc5ccaccd996ade4f34
Atanasius01 <atanasius256@gmail.com>
2023-03-24 08:39:14 +0700

    Latihan php

---
+++ latihanphp5.php
@@ -0,0 +1,17 @@
+<?php
+$nama = "Atan";
+$nilai = 90;
+
+if ($nilai >= 85 && $nilai <=100) $grade = "A";
+else if ($nilai >= 75 && $nilai <=84) $grade = "B";
+else if ($nilai >= 60 && $nilai <=74) $grade = "C";
+else if ($nilai >= 30 && $nilai <=59) $grade = "D";
+else if ($nilai >= 0 && $nilai <=29) $grade = "E";
+else $grade = "";
+
+
+?>
+
+Nama : <?= $nama ?>
+<br> Nilai : <?= $nilai ?>
+<br> Grade : <?=$grade?>
