3a294fb5282a161a4584cfa4c867acc77fa26945
Atanasius01 <atanasius256@gmail.com>
2023-03-30 20:05:30 +0700

    Tugas 5 PHP

---
+++ Tugas5PHP.php/objekPegawai.php
@@ -0,0 +1,19 @@
+<?php
+require 'pegawai.php';
+
+$pegawai1 = new pegawai('01001', 'Atanasius', 'Manager', 'Islam', 'Menikah');
+$pegawai2 = new pegawai('01002', 'Rohendi', 'Asisten Manager', 'Islam', 'Belum Menikah');
+$pegawai3 = new pegawai('01003', 'Yuda Permana', 'Kepala Bagian', 'Islam', 'Menikah');
+$pegawai4 = new pegawai('01004', 'Debora', 'Asisten Manager', 'Kristen', 'Belum Menikah');
+$pegawai5 = new pegawai('01005', 'Alfi', 'Staff', 'Islam', 'Menikah');
+$pegawai6 = new pegawai('01006', 'Karyn', 'Manager', 'Islam', 'Belum Menikah');
+$pegawai7 = new pegawai('01007', 'Diyah', 'Kepala Bagian', 'Islam', 'Menikah');
+
+$ar_data= [$pegawai1, $pegawai2, $pegawai3,$pegawai4,$pegawai5, $pegawai6, $pegawai7];
+
+foreach($ar_data as $dp){
+    $dp->cetak();
+}
+
+
+?>
